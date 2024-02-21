3f4090b63bc8d5a4d90e0f831327accd931b59a8
AishwaryaWKulkarni <awkulkarni2002@gmail.com>
2024-02-21 12:00:18 +0530

    [St No-11] initial commit for data jpa app

---
+++ springdatajpaapp/springdatajpaapp/src/main/java/com/csi/SpringdatajpaappApplication.java
@@ -0,0 +1,13 @@
+package com.csi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdatajpaappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdatajpaappApplication.class, args);
+	}
+
+}
